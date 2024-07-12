21914aa0e890b6b688e4d7f55b5ee3ef9b2feacb
Yash Kumar Sharma <yks.nit@gmail.com>
2024-07-12 05:52:51 +0400

    fix: add missing StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
-import { Provider } from 'react-redux'
-import { useStore } from 'container/StoreContext'
+import { useSelector } from 'react-redux'
 import { AuthProvider } from 'container/AuthContext'
+import { StoreProvider } from 'container/StoreContext'
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
@@ -10,34 +10,19 @@ import i18n from './translations/i18n'
 
 import './translations/i18n'
 
-// Todo: Leaving some commented tested code for redux integration.
-// const rootHelloReducer = (state = {
-//   isLoading: true,
-// }, action) => state
-
 const App = () => {
-  // const { store, replaceReducertest } = useStore()
-  const { store } = useStore()
-
-  // const language = useSelector(state => state.appSettings.language)
-
-  // Todo: Move to a separate component under <Provider>, as it is very inefficient
-  const language = store.getState()?.appSettings?.language || 'en'
+  const language = useSelector(state => state.appSettings?.language || 'en')
 
   useEffect(() => {
     i18n.changeLanguage(language)
   }, [language])
 
-  // useEffect(() => {
-  //   if(!replaceReducertest) return
-  //   replaceReducertest({
-  //     rootHelloReducer: rootHelloReducer,
-  //     // Add other reducers here if needed
-  //   })
-  // }, [replaceReducertest])
+  useEffect(() => {
+    i18n.changeLanguage(language)
+  }, [language])
 
   return (
-    <Provider store={store}>
+    <StoreProvider>
       <AuthProvider>
         <Router basename='/auth'>
           <Routes>
@@ -46,7 +31,7 @@ const App = () => {
           </Routes>
         </Router>
       </AuthProvider>
-    </Provider>
+    </StoreProvider>
   )
 }
 
